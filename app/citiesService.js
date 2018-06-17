7c5b569fb542a93ed1430b6ca086fcabc782d7ac
Norbert Spieß <norbertspiess@users.noreply.github.com>
2018-06-17 20:44:36 +0200

    reintegrate coordinate service into the cities service, as the logic is only used there

--- app/citiesService.js
+++ app/citiesService.js
@@ -1,5 +1,4 @@
 var BadRequestError = require('restify-errors').BadRequestError;
-var coordinateService = require('./coordinateService');
 var request = require('request');
 var sprintf = require('sprintf-js').sprintf;
 var openWeatherConfig = require('./openWeatherConfig');
@@ -10,13 +9,13 @@ function getCities(req, res, next) {
     var latitude = parseFloat(req.query.lat);
     var longitude = parseFloat(req.query.lon);
 
-    if (!coordinateService.validCoordinates(latitude, longitude)) {
+    if (!validCoordinates(latitude, longitude)) {
         console.log('parameters invalid, sending 400');
         res.send(new BadRequestError('lat/lng required'));	
         return next();
     }
 
-    var box = coordinateService.calculateBoundingBoxOf10KmAround(latitude, longitude);
+    var box = calculateBoundingBoxOf10KmAround(latitude, longitude);
 
     var params = sprintf('box/city?bbox=%f,%f,%f,%f,10&appid=%s',
         box.leftLongitude, box.bottomLatitude, box.rightLongitude, box.topLatitude,
@@ -55,6 +54,24 @@ function getCities(req, res, next) {
         res.send(new Error('internal error'));
         next();
     });
+}
+
+function calculateBoundingBoxOf10KmAround(latitude, longitude) {
+    let earthRadius = 40075;
+    let latitudeModifier = 5 * (360 / earthRadius);
+    let longitudeModifier = 5 * (360 / (Math.cos(latitude) * earthRadius));
+    return {
+        leftLongitude: longitude - longitudeModifier,
+        rightLongitude: longitude + longitudeModifier,
+        topLatitude: latitude + latitudeModifier,
+        bottomLatitude: latitude - latitudeModifier
+    };
+}
+
+function validCoordinates(latitude, longitude) {
+    var latitudeValid = latitude <= 90 && latitude >= -90;
+    var longitudeValid = longitude <= 180 && longitude >= -180;
+    return latitudeValid && longitudeValid;
 }
 
 module.exports = {
